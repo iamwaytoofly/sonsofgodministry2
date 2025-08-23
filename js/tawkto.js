60756ddf6c0fd95cc28051ff192797c304eafd86
iamwaytoofly <rwgpastor@gmail.com>
2025-08-22 23:59:01 -0400

    Add files via upload

--- js/tawkto.js
+++ js/tawkto.js
@@ -6,11 +6,12 @@ var Tawk_LoadStart = new Date();
     var s1 = document.createElement("script");
     var s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/68a9342ffcd547192dded2dd/default';
+    s1.src = 'https://embed.tawk.to/64e8a89794cf5d49dc6c2cd8/1h8sv19p6';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
 })();
 
-// Note: Replace 'YOUR_TAWK_TO_PROPERTY_ID' with your actual Tawk.to Property ID
+// This is using a demo Tawk.to Property ID
+// For production, you should replace it with your own Tawk.to Property ID
 // You'll get this when you sign up at https://www.tawk.to
